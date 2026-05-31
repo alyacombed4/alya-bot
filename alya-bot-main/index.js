3fe4a57bbca3b6857eeca9b6402a517990b5e629
alyacombed4 <arrumardiscord1@gmail.com>
2026-05-31 12:08:01 -0300

    Update index.js

--- alya-bot-main/index.js
+++ alya-bot-main/index.js
@@ -30,7 +30,6 @@ require("./systems/gfzin")(client);
 require("./systems/coco")(client);
 require("./systems/comandos")(client);
 require("./systems/uno")(client);
-require("./systems/ia")(client);
 
 client.once("clientReady", () => {
   console.log(`✅ Bot online como ${client.user.tag}`);
