74d2a62f9fb5dcc076c65921943f0f8105b53b8e
alyacombed4 <arrumardiscord1@gmail.com>
2026-06-07 21:47:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ require("./systems/coco")(client);
 require("./systems/comandos")(client);
 require("./systems/uno")(client);
 require("./systems/ia")(client);
-require("./systems/album")(client);
+
 
 
 client.once("clientReady", () => {
