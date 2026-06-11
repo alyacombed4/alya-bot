6ac5452882327215bed55a10eddad1ffe0faa1b5
alyacombed4 <arrumardiscord1@gmail.com>
2026-06-11 13:08:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ require("./systems/coco")(client);
 require("./systems/comandos")(client);
 require("./systems/uno")(client);
 require("./systems/ia")(client);
-require("./systems/tarefas")(client);
+require("./systems/tarefas").setup(client);
 require("./systems/Kahoot").setup(client);
 
 
